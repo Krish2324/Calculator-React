cbb88d2fa47c0555f096f5c2c834ebff522a4959
Krish2324 <krack232004@gmail.com>
2023-08-24 17:27:33 +0530

    Calculator File Added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Calculator from './component/calculator';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "container" >
+        <
+        Calculator / >
+        <
+        /div>
+    );
+}
+
+export default App;
