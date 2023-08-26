a0ee3ad5b7d4443fc89481bfc6609772b8fe5a70
Krish2324 <krack232004@gmail.com>
2023-08-26 18:00:06 +0530

    Calculator Style

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Button } from "react-bootstrap";
 
 function Calculator() {
-    return ( <
-        div > Calculator < /div>
+    return ( 
+    <div className = 'Calculator' >
+        <div className = 'Calculator-Wrapper' >
+            <div className = 'Calculator-Heading'>
+                <h3 className = "disable">Calculator In React</h3>
+            </div>
+
+            <div className='Calculator-Screen'>
+                <div className='Calculator-Output'>
+                    <span>
+
+                    </span>
+                </div>
+
+                <div className = 'Calculator-Backspace'>
+                    <button className='Backspace-Btn'>
+                        <i className=''></i>
+                    </button>
+                    <span>
+
+                    </span>
+                </div>
+
+                <div className='Calculator-Button'>
+                    <Button type='button' className='Top-Btn' value="AC">
+                        AC
+                    </Button>
+                    <Button type='button' className='Top-Btn' value="PM">
+                        &plusmn;
+                    </Button>
+                    <Button type='button' className='Top-Btn' value="%">
+                        %
+                    </Button>
+                    <Button type='button' className='Top-Btn' value="">
+                        <i className='fa-solid fa-rotate-left'></i>
+                    </Button>
+                </div>
+            </div>
+        </div>
+    </div>
     )
 }
 
